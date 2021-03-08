65ef157fcce55725c0080a401f29f8c2b049b535
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2021-03-08 18:39:41 +0700

    Add Hello World

---
+++ FileLab/FileLab/ReadFile.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FileLab
+{
+    partial class ReadFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnFileStream = new System.Windows.Forms.Button();
+            this.btnStreamReader = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFileReadAsync = new System.Windows.Forms.Button();
+            this.btnStreamReadSync = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 14);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(675, 403);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnFileStream
+            // 
+            this.btnFileStream.Location = new System.Drawing.Point(722, 18);
+            this.btnFileStream.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFileStream.Name = "btnFileStream";
+            this.btnFileStream.Size = new System.Drawing.Size(142, 47);
+            this.btnFileStream.TabIndex = 1;
+            this.btnFileStream.Text = "FileStream";
+            this.btnFileStream.UseVisualStyleBackColor = true;
+            this.btnFileStream.Click += new System.EventHandler(this.btnFileStream_Click);
+            // 
+            // btnStreamReader
+            // 
+            this.btnStreamReader.Location = new System.Drawing.Point(723, 86);
+            this.btnStreamReader.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStreamReader.Name = "btnStreamReader";
+            this.btnStreamReader.Size = new System.Drawing.Size(140, 49);
+            this.btnStreamReader.TabIndex = 2;
+            this.btnStreamReader.Text = "Stream Reader";
+            this.btnStreamReader.UseVisualStyleBackColor = true;
+            this.btnStreamReader.Click += new System.EventHandler(this.btnStreamReader_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(725, 298);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(137, 44);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFileReadAsync
+            // 
+            this.btnFileReadAsync.Location = new System.Drawing.Point(725, 159);
+            this.btnFileReadAsync.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFileReadAsync.Name = "btnFileReadAsync";
+            this.btnFileReadAsync.Size = new System.Drawing.Size(137, 46);
+            this.btnFileReadAsync.TabIndex = 4;
+            this.btnFileReadAsync.Text = "FileReadAsync";
+            this.btnFileReadAsync.UseVisualStyleBackColor = true;
+            this.btnFileReadAsync.Click += new System.EventHandler(this.btnFileReadAsync_Click);
+            // 
+            // btnStreamReadSync
+            // 
+            this.btnStreamReadSync.Location = new System.Drawing.Point(725, 230);
+            this.btnStreamReadSync.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStreamReadSync.Name = "btnStreamReadSync";
+            this.btnStreamReadSync.Size = new System.Drawing.Size(137, 44);
+            this.btnStreamReadSync.TabIndex = 5;
+            this.btnStreamReadSync.Text = "StreamReadAsync";
+            this.btnStreamReadSync.UseVisualStyleBackColor = true;
+            this.btnStreamReadSync.Click += new System.EventHandler(this.btnStreamReadAsync_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(727, 358);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ReadFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 443);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStreamReadSync);
+            this.Controls.Add(this.btnFileReadAsync);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStreamReader);
+            this.Controls.Add(this.btnFileStream);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ReadFile";
+            this.Text = "ReadFile";
+            this.Load += new System.EventHandler(this.ReadFile_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnFileStream;
+        private System.Windows.Forms.Button btnStreamReader;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFileReadAsync;
+        private System.Windows.Forms.Button btnStreamReadSync;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ FileLab/FileLab/ReadFile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace FileLab
+{
+    public partial class ReadFile : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        FileStream fs;
+        string content;
+        byte[] bytes;
+
+        public ReadFile()
+        {
+            InitializeComponent();
+        }
+        // Cách đọc sử dụng FileStream
+        private void btnFileStream_Click(object sender, EventArgs e)
+        {
+            // Hiển thị Hộp thoại OpenFileDialog cho phép chọn 1 file
+            // Thuộc tính FileName của OpenFileDialog trả về đường dẫn của file
+            ofd.ShowDialog();
+            //
+        }
+
+        // Cách đọc sử dụng StreamReader
+        private void btnStreamReader_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private async void btnFileReadAsync_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private async void btnStreamReadAsync_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void ReadFile_Load(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
+
+
