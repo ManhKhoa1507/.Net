1a1abdb9c10be300222b9038f5d1f5e359690e04
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2021-03-10 18:32:44 +0700

    Add the Async FileReader and StreamReader

--- FileLab/FileLab/ReadFile.cs
+++ FileLab/FileLab/ReadFile.cs
@@ -25,47 +25,90 @@ namespace FileLab
         {
             InitializeComponent();
         }
-        // Cách đọc sử dụng FileStream
+        // Cách đọc sử dụng FileStream Sync
         private void btnFileStream_Click(object sender, EventArgs e)
         {
+
             // Hiển thị Hộp thoại OpenFileDialog cho phép chọn 1 file
             // Thuộc tính FileName của OpenFileDialog trả về đường dẫn của file
             ofd.ShowDialog();
-            fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
-
-            // 1 bytes la 1 chu cai 
-            bytes = new byte[fs.Length];
-
-            // Doc file tu vi tri dau tien den het file
-            fs.Read(bytes, 0, (int)fs.Length);
             
-            content = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
-            richTextBox1.Text = content;
+            // Use using to not to close the fs
+            using (FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate))
+            {
+                int fsLength = (int)fs.Length;
+                // 1 bytes = 1 word
+                bytes = new byte[fsLength];
+
+                // Read file from begin to end
+                fs.Read(bytes, 0, fsLength);
+            }
+
+            // Encoding the content in the file
+            // UTF-8 supported the Vietnamese
+            string content = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            Display_richTextBox(content);
             //
         }
 
-        // Cách đọc sử dụng StreamReader
+        // Cách đọc sử dụng StreamReader Sync
         private void btnStreamReader_Click(object sender, EventArgs e)
         {
             ofd.ShowDialog();
-            StreamReader StreamFile = new StreamReader(ofd.FileName);
-            richTextBox1.Text = StreamFile.ReadToEnd();
+            
+            using (StreamReader sr = new StreamReader(ofd.FileName))
+            {
+                int srLength = (int)sr.BaseStream.Length;
+                
+                // Create a byte of char in streamfile array 
+                // Read to end of file
+                Char[] bytes = new char[srLength];
+                sr.Read(bytes, 0, srLength);
+            }
+
+            string content = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            Display_richTextBox(content);
         }
 
+        // Read the file using FileStream Async
+        private async void btnFileReadAsync_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog();
+
+            using (FileStream fs = File.Open(ofd.FileName, FileMode.Open))
+            {
+                int fsLength = (int)fs.Length;
+                bytes = new byte[fsLength];
+
+                // Await for read the file async 
+                // Read to end of file
+                await fs.ReadAsync(bytes, 0, fsLength);
+            }
+            string content = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            Display_richTextBox(content);
+        }
+
+        // Read the file using StreamReader Async
+        private async void btnStreamReadAsync_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog(); 
+
+            using (StreamReader sr = new StreamReader(ofd.FileName))
+            {
+                int srLength = (int)sr.BaseStream.Length;
+
+                Char[] bytes = new Char[srLength];
+                await sr.ReadAsync(bytes, 0, srLength);
+            }
+
+            string content = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            Display_richTextBox(content);
+        }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            // Clear the richTextBox
             richTextBox1.Text = "";
-        }
-
-        private async void btnFileReadAsync_Click(object sender, EventArgs e)
-        {
-            //
-        }
-
-        private async void btnStreamReadAsync_Click(object sender, EventArgs e)
-        {
-            //
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -76,6 +119,11 @@ namespace FileLab
         private void ReadFile_Load(object sender, EventArgs e)
         {
             //
+        }
+
+        public void Display_richTextBox(string content)
+        {
+            richTextBox1.Text = content;
         }
     }
 }
