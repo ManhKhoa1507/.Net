92652c1688ee6130aea050c59e62f37e460c293a
ManhKhoa1507 <nguyenlemanhkhoa15072001@gmail.com>
2021-04-25 13:40:17 +0700

    Add Thread

---
+++ Thread/Thread/Source.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create the Thread 
+        Thread t = new Thread ( new ThreadStart(MethodA));
+        t.Start();
+        
+        MethodB();
+        Console.ReadKey();
+    }
+
+    static void MethodA()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.Write("0");
+    }
+    static void MethodB()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.Write("1");
+    }
+}
